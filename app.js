e307b6be76487da68ca331f672f2901fc63071f1
JCedilla <jaredchristiancedilla1@gmail.com>
2024-09-09 22:18:39 +0800

    Added functional reset button, changed button CSS added socials icon animation on hover changed some fonts and text-colors

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const cookieButton = document.getElementById('cookie-button');
+const resetButton = document.getElementById('reset-button');
 const cookieDisplay = document.querySelector('.cookie-display');
-
 let cookieCount = 0;
 
 // Function to handle button clicks
-function handleButtonClick() {
-    cookieCount += 1; // Increment cookie count
-
-    // Check if cookie count exceeds 100
-    if (cookieCount > 100) {
-        cookieDisplay.textContent = 'Good job on clicking 100 times! your next click will reset the counter.'; // Display message
-        cookieCount = 0; // Reset the count
-    } else {
-        cookieDisplay.textContent = cookieCount; // Display the count
-    }
+cookieButton.onclick = function(){
+    cookieCount++; // Increment cookie count
+    cookieDisplay.textContent = cookieCount; 
 }
 
-cookieButton.addEventListener('click', handleButtonClick);
+resetButton.onclick = function(){
+    cookieCount = 0;
+    cookieDisplay.textContent = 0;
+}
+
+
 
 
 
